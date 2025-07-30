4cd953f332a6f39957401b394127b69a8e4cbe28
JoyadityaNath <nathja@rknec.edu>
2025-07-30 21:15:40 +0530

    Final Code

--- imageGallery/src/App.jsx
+++ imageGallery/src/App.jsx
@@ -1,14 +1,43 @@
 import { useState } from 'react'
+import sculptureList from './data'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default  function App() {
+  const [nextIndex,setNextIndex]=useState(0);
+  const [showDetails,setShowDetails]=useState(false);
+
+  const handleNext = () => {
+    setNextIndex((prevIndex) => (prevIndex + 1) % sculptureList.length);
+};
+
+const handlePrev=()=>{
+  setNextIndex((prevIndex)=>((prevIndex-1)+sculptureList.length)%sculptureList.length)
+}
+
+const handleDetails=()=>{
+  setShowDetails((prevState)=>!prevState)
+}
+
 
   return (
     <>
+    <button className='next' onClick={handlePrev}>Previous</button>
+     <button className='next' onClick={handleNext}>Next</button>
      
+      <h2><strong>{sculptureList[nextIndex].name}</strong></h2>
+      <p>{`(${nextIndex+1} of ${sculptureList.length})`}</p>
+      <button className='details' onClick={handleDetails}>Show Details</button>
+      {showDetails ? (
+  <>
+    <p>{sculptureList[nextIndex].description}</p>
+  </>
+) : null}
+
+<img
+      src={sculptureList[nextIndex].url}
+      alt={sculptureList[nextIndex].alt}
+    /> 
     </>
   )
 }
 
-export default App
